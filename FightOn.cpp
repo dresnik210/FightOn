bfb14566e78f98186f1d722ec52ffceab0f530f4
Dylan Resnik <dylanres@usc.edu>
2013-01-18 11:21:21 -0800

    Created the FightOn HelloWorld-like application

---
+++ FightOn.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FightOn
+//
+//  Created by Dylan Resnik on 1/18/13.
+//  Copyright (c) 2013 Dylan Resnik. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    cout << "Fight On!" << endl;
+    return 0;
+}
+
